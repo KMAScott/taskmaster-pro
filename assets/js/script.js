6efae640d98f5b0c95b983c75cc1b72c8be73a2d
Scott Ashley <burnbright987@gmail.com>
2022-04-27 10:29:21 -0400

    added drag and drop

--- assets/js/script.js
+++ assets/js/script.js
@@ -175,6 +175,57 @@ $("#remove-tasks").on("click", function() {
   saveTasks();
 });
 
+$(".card .list-group").sortable({
+  connectWith: $(".card .list-group"),
+  scroll: false,
+  tolerance: "pointer",
+  helper: "clone",
+
+  update: function(event) {
+    var tempArr =[];
+
+    $(this).children().each(function(){
+      var text = $(this)
+      .find("p")
+      .text()
+      .trim();
+
+      var date = $(this)
+      .find("span")
+      .text()
+      .trim();
+
+      tempArr.push({
+        text: text,
+        date: date
+      });
+      var arrName = $(this)
+        .attr("id")
+        .replace("list-", "");
+
+      tasks[arrName] = tempArr;
+      saveTasks();
+    });
+
+    console.log(tempArr);
+  }
+});
+
+$("#trash").droppable({
+  accept: ".card .list-group-item",
+  tolerance: "touch",
+  drop: function(event, ui) {
+    ui.draggable.remove();
+    console.log("drop");
+  },
+  over: function(event, ui) {
+    console.log("over");
+  },
+  out: function(event, ui) {
+    console.log("out");
+  }
+})
+
 // load tasks for the first time
 loadTasks();
 
